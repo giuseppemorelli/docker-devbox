4dab808233380d882518a6beb40e6cfa158c9039
Giuseppe <info@giuseppemorelli.net>
2020-06-27 11:32:13 +0200

    add provision (commented) for xdebug error with php 7.3

--- local/provision/bin/sudo_web_tasks.sh
+++ local/provision/bin/sudo_web_tasks.sh
@@ -4,6 +4,11 @@ set -e
 
 # apt-get update && apt-get install -y nano
 
+# fix Header may not contain NUL bytes in Unknown on line 0
+# install 2.9 version
+# pecl config-set php_ini  /etc/php/7.3/apache2/php.ini
+# pecl install xdebug
+
 cp /var/www/html/docker/local/config/php/7.2/mods-available/devbox.ini /etc/php/7.2/apache2/conf.d/devbox.ini
 cp /var/www/html/docker/local/config/php/7.2/mods-available/devbox.ini /etc/php/7.2/cli/conf.d/devbox.ini
 
