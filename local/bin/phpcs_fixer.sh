0cefcb58ad86b176a41a7340df39844b00c6ef22
Giuseppe <info@giuseppemorelli.net>
2020-08-07 19:17:14 +0200

    add script for php quality tools and composer used for external software like phpstorm

---
+++ local/bin/phpcs_fixer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKER_COMPOSE_DIR=${SCRIPTDIR}/../
+
+cd "$DOCKER_COMPOSE_DIR" || exit
+docker-compose exec -T -u $UID web /var/www/html/project/vendor/bin/phpcbf "$@"
