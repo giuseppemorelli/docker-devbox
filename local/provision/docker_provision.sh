6c2a548c56b37dc88dd0da0176b30ca616a285a0
Giuseppe Morelli <info@giuseppemorelli.net>
2020-12-21 17:25:27 +0100

    disable xdebug on provisioning scripts

--- local/provision/docker_provision.sh
+++ local/provision/docker_provision.sh
@@ -3,6 +3,7 @@
 set -e
 
 docker-compose exec web bash /var/www/html/docker/local/provision/bin/sudo_web_tasks.sh
+docker-compose exec web bash /var/www/html/docker/local/provision/bin/disable_xdebug.sh
 docker-compose exec mysql bash /devbox/provision/bin/db_tasks.sh
 docker-compose exec -u $UID web bash /var/www/html/docker/local/provision/bin/web_tasks.sh
 docker-compose restart
