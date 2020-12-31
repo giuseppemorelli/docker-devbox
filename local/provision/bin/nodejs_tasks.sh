00340a2b2175bed8ae59cfdba1cb399a2c034782
Giuseppe Morelli <info@giuseppemorelli.net>
2020-12-31 17:04:31 +0100

    add docker provision for nodejs env

---
+++ local/provision/bin/nodejs_tasks.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /home/node/project
+npm install
--- local/provision/docker_provision.sh
+++ local/provision/docker_provision.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-docker-compose exec web bash /var/www/html/docker/local/provision/bin/sudo_web_tasks.sh
-docker-compose exec web bash /var/www/html/docker/local/provision/bin/disable_xdebug.sh
-docker-compose exec mysql bash /devbox/provision/bin/db_tasks.sh
-docker-compose exec -u $UID web bash /var/www/html/docker/local/provision/bin/web_tasks.sh
+## FOR LAMP STACK
+#docker-compose exec web bash /var/www/html/docker/local/provision/bin/sudo_web_tasks.sh
+#docker-compose exec web bash /var/www/html/docker/local/provision/bin/disable_xdebug.sh
+#docker-compose exec mysql bash /devbox/provision/bin/db_tasks.sh
+#docker-compose exec -u $UID web bash /var/www/html/docker/local/provision/bin/web_tasks.sh
+
+## FOR NODE.JS STACK
+# docker-compose run nodejs bash /home/node/docker/local/provision/bin/nodejs_tasks.sh
+
 docker-compose restart
